7c10c768d8c5a6b5c0a579df20ec2436d787530a
Maarten de Boer <maarten@djambu.com>
2014-04-14 10:02:59 +0200

    item and task data models edited

--- api/models/Item.js
+++ api/models/Item.js
@@ -1,15 +1,21 @@
-/**
- * Item.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs		:: http://sailsjs.org/#!documentation/models
- */
-
 module.exports = {
-
-	attributes: {
-
-
-	}
-
+  schema: true,
+  attributes: {
+    role: {
+      type: 'string',
+      required: true
+    },
+    feature: {
+      type: 'string',
+      required: true
+    },
+    status: {
+      type: 'string',
+      defaultsTo: ''
+    },
+    tasks:{
+      collection: 'task',
+      via: 'owner'
+    }
+  }
 };
--- api/models/Task.js
+++ api/models/Task.js
@@ -1,15 +1,16 @@
-/**
- * Task.js
- *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
- * @docs		:: http://sailsjs.org/#!documentation/models
- */
-
 module.exports = {
-
-	attributes: {
-
-
-	}
-
+  schema: true,
+  attributes: {
+    title: {
+      type: 'string',
+      required: true
+    },
+    assignee: {
+      type: 'string',
+      required: false
+    },
+    owner:{
+      model: 'item'
+    }
+  }
 };
