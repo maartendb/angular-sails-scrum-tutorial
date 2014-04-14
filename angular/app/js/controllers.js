26c5be4be6fbb57942f9194f0b7f310e976076ba
Maarten de Boer <maarten@djambu.com>
2014-04-14 22:40:49 +0200

    updates working, indicate pending updates with red buttons

--- angular/app/js/controllers.js
+++ angular/app/js/controllers.js
@@ -8,7 +8,6 @@ myAppControllers.controller('MyCtrl1',
 ['$scope','$sails','$filter',
 function($scope,$sails,$filter) {
   ///////////
-  $scope.lookup = {};
   $scope.items = [];
 
   $scope.createItem = function(newItem) {
@@ -36,29 +35,84 @@ function($scope,$sails,$filter) {
     });
     item.newTask = {};
   };
+  $scope.taskEdited = function(task)
+  {
+    task.edited = 'edited';
+  }
+  $scope.itemEdited = function(item)
+  {
+    item.edited = 'edited';
+  }
   $scope.updateItem = function(item) {
+    delete item.edited;
     $sails.put("/item/"+item.id,item).success(function (response) {
-      console.log("put /task success");
+      console.log("put /item success");
     }).error(function (response) {
-      console.log('put /task error');
+      console.log('put /item error');
     });
   };
   $scope.updateTask = function(task) {
+    delete task.edited;
     $sails.put("/task/"+task.id,task).success(function (response) {
       console.log("put /task success");
     }).error(function (response) {
       window.alert('put /task error');
     });
   };
+
+  $scope.getItemById = function(id) {
+    for (var i in $scope.items)
+    {
+      if ($scope.items[i].id == id)
+      {
+        return $scope.items[i];
+      }
+    }
+    return null;
+  };
+
+  $scope.setItemById = function(id,data) {
+    for (var i in $scope.items)
+    {
+      if ($scope.items[i].id == id)
+      {
+        $scope.items[i] = data;
+        return;
+      }
+    }
+  };
+   
+  $scope.getTaskById = function(id) {
+    for (var ii in $scope.items)
+    {
+      for (var ti in $scope.items[ii].tasks)
+      {
+        if ($scope.items[ii].tasks[ti].id == id)
+        {
+          return $scope.items[ii].tasks[ti];
+        }
+      }
+    }
+    return null;
+  };
+
+  $scope.setTaskById = function(id,data) {
+    for (var ii in $scope.items)
+    {
+      for (var ti in $scope.items[ii].tasks)
+      {
+        if ($scope.items[ii].tasks[ti].id == id)
+        {
+          $scope.items[ii].tasks[ti] = data;
+          return;
+        }
+      }
+    }
+  };
+ 
   (function() {
     $sails.get("/item").success(function (response) {
       $scope.items = response; 
-      $scope.lookup = {};
-      for (var i in $scope.items)
-      {
-        $scope.lookup[$scope.items[i].id] = i;
-        console.log(  $scope.lookup[$scope.items[i].id] );
-      }
     }).error(function (response) { console.log('error');});
     
     $sails.get("/task").success(function (response) {
@@ -66,44 +120,49 @@ function($scope,$sails,$filter) {
     }).error(function (response) { console.log('error');});
 
     $sails.on('item', function ( message ) {
-      window.alert('sails published a message for item: '+message.verb);
+      console.log('sails published a message for item: '+message.verb);
       switch (message.verb)
       {
+        case 'updated':
+          $scope.setItemById(message.id,message.data);
+          break;
         case 'created':
           console.log("pushing "+JSON.stringify(message.data));
           $scope.items.push(message.data);
-          $scope.lookup = {};
-          for (var i in $scope.items)
-          {
-            $scope.lookup[$scope.items[i].id] = i;
-          }
           break;
         case 'destroyed':
           $scope.items = $scope.items.filter(function(item) {
             return item.id != message.id;
           });
-          $scope.lookup = {};
-          for (var i in $scope.items)
-          {
-            $scope.lookup[$scope.items[i].id] = i;
-          }
           break;
         case 'addedTo':
-          var idx = $scope.lookup[message.id];
-          $sails.get("/task/"+message.addedId).success(function (aTask) {
-            $scope.items[idx].tasks.push(aTask);
-          }).error(function (aTask) { window.alert('error getting added task');});
+          var item = $scope.getItemById(message.id);
+          if (item != null)
+          {
+            $sails.get("/task/"+message.addedId).success(function (aTask) {
+              item.tasks.push(aTask);
+            }).error(function (aTask) { window.alert('error getting added task');});
+          }
           break;
         case 'removedFrom':
-          var idx = $scope.lookup[message.id];
-          $scope.items[idx].tasks = $scope.items[idx].tasks.filter(function(task) {
-            return task.id != message.removedId;
-          });
+          var item = $scope.getItemById(message.id);
+          if (item != null)
+          {
+            item.tasks = item.tasks.filter(function(task) {
+              return task.id != message.removedId;
+            });
+          }
           break;
       }
     });
     $sails.on('task', function ( message ) {
       console.log('sails published a message for task: '+message.verb);
+      switch (message.verb)
+      {
+        case 'updated':
+          $scope.setTaskById(message.id,message.data);
+          break;
+      }
     });
 
   })();
